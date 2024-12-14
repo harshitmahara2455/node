ba7ca92dd52f5f0975d550c563395af9566473ca
harshit <harshitmahara20@gmail.com>
2024-12-14 19:46:54 +0530

    learnt about globals

---
+++ 2-global.js
@@ -0,0 +1,10 @@
+//Global == no window !!!
+
+// __dirname == name of the directory 
+// __filename == file name 
+// require ==function to use modules 
+//module == info about current module 
+//process == info about where the program is being executed 
+
+
+console.log(__dirname);
