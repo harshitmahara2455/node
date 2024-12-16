d80ff3e5c60ce0ab1fb6b06c93016a982b404dcc
harshit <harshitmahara20@gmail.com>
2024-12-17 01:47:51 +0530

    crazy padai chalri

---
+++ 3-modules.js
@@ -0,0 +1,11 @@
+
+const names = require('./4-names')
+const sayHi= require('./5-utils')
+const data = require('./6-alternativeflovour')
+require('./7-mindgranade.js')
+console.log( data); 
+
+sayHi(names.john)
+sayHi(names.peter)
+
+
--- app.js
+++
@@ -1,9 +0,0 @@
-
-const names = require('./4-names')
-const sayHi= require('./5-utils')
-const data = require('./6-alternativeflovour')
-
-console.log( data); 
-
-sayHi(names.john)
-sayHi(names.peter)
